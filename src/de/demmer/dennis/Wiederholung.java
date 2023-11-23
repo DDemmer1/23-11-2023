794377a8b96fbeafc301bdba9dfe0ca4d1a4c5b1
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-11-23 17:29:26 +0100

    initial commit

---
+++ src/de/demmer/dennis/Wiederholung.java
@@ -0,0 +1,53 @@
+package de.demmer.dennis;
+
+
+// ALT + SHIFT + R -> überall namen ändern
+public class Wiederholung {
+	
+	public static void main(String[] args) {
+	
+		
+		//-- while --
+		
+		//Läufervariable
+		int i = 0;
+	
+			//Abbruchbedingung
+		while(i < 10) {
+
+			System.out.println(i);
+			
+			//hochzählen der Läuervariable
+			i = i + 1;
+		}
+		
+		
+		
+		//-- for --
+
+		      //Läufervariable    //Abbruchbedingung    //hochzählen der Läuervariable
+		for(  int j = 5        ;     j < 10          ;                j++            ) {
+			
+			
+			
+			
+			
+		}
+		
+		
+	// do - while
+		
+	int x = 0;
+	do {
+		System.out.println("test");
+		x++;
+	} while(x < 10);
+		
+
+		
+
+		
+		
+	}
+
+}
